45d87fa0e4ab15c0a511bac6110401594a990971
c0sm1k <vasily.trudkoff@yandex.ru>
2026-03-05 09:25:16 +0300

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -11,7 +11,10 @@ class RoleManager(commands.Cog):
         rolenames = [role.name for role in interaction.user.roles[1:]]
         await interaction.response.send_message(f"your roles: {rolenames}", ephemeral=True)
 
-            
+    @commands.slash_command(name='забрать_роль', description='Отзыв роли у пользователя')
+    async def take_role(interaction, member: disnake.Member, role: disnake.Role):
+        await member.remove_roles(role)
+        await interaction.response.send_message('Роль отозвана!')
 
 def setup(bot):
     bot.add_cog(RoleManager(bot))
